e3edeb154a22e9ae68b73533268238473d2be748
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-01 17:33:39 -0700

    create global variables linked to HTML, and also created a function to start the game, the function will hide the Start button and then, make the question visible

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// create variables to connect to the buttons
+    // *start-btn
+var startBtn = document.getElementById("start-btn");
+    // *next-btn
+var nextBtn = document.getElementById("next-btn");
+    // *score-btn
+var scoreBtn = document.getElementById("score-btn");
+
+var questionCardEle = document.getElementById("question-card");
+
+startBtn.addEventListener("click",startGame); //eventListener to activate start btn, it also call the function startGame
+
+    // hold the score
+    // count the score
+
+
+// function to start the game
+function startGame(){
+    startBtn.classList.add("hide");
+
+    questionCardEle.classList.remove("hide");
+
+
+}
+
+// function to move to  the next question
+
+
+// time counter 
+
+
+// create a function to random select a question  
+
+// create an object array that contain the questions and answers 
+
